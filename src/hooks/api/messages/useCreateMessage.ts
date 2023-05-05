03835390c6dc3351cd334f842851fa476076d022
Eduardo de Paula <eduardoibarr56@gmail.com>
2023-05-05 16:11:24 -0300

    feat: adding messages api hooks

---
+++ src/hooks/api/messages/useCreateMessage.ts
@@ -0,0 +1,6 @@
+import { useMutation } from 'react-query';
+import { messagesService } from '../../../services/api';
+
+export function useCreateMessage() {
+  return useMutation(messagesService.addMessage);
+}
---
+++ src/hooks/api/messages/useShowMessage.ts
@@ -0,0 +1,6 @@
+import { useQuery } from 'react-query';
+import { messagesService } from '../../../services/api';
+
+export function useShowMessage(id: string) {
+  return useQuery('showMessage', () => messagesService.showMessage(id));
+}
