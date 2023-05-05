67dd30464d2212b03801e48a9478355b166dbace
Eduardo de Paula <eduardoibarr56@gmail.com>
2023-05-05 16:10:51 -0300

    feat: adding agents api hooks

---
+++ src/hooks/api/agents/useShowAgent.ts
@@ -0,0 +1,6 @@
+import { useQuery } from 'react-query';
+import { agentsService } from '../../../services/api';
+
+export function useShowAgent(id: string) {
+  return useQuery('showAgent', () => agentsService.showAgent(id));
+}
---
+++ src/hooks/api/agents/useUpdateAgent.ts
@@ -0,0 +1,9 @@
+import { useMutation } from 'react-query';
+import { agentsService } from '../../../services/api';
+import { IUpdateAgent } from '../../../interfaces/update';
+
+export function useUpdateAgent(id: string) {
+  return useMutation((values: IUpdateAgent) =>
+    agentsService.updateAgent(values, id)
+  );
+}
