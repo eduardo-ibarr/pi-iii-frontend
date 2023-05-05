933d9d0ee3cb1699eaa607a675049434cca4a55f
Eduardo de Paula <eduardoibarr56@gmail.com>
2023-05-05 16:11:42 -0300

    feat: adding tickets api hooks

---
+++ src/hooks/api/tickets/useCreateTicket.ts
@@ -0,0 +1,6 @@
+import { useMutation } from 'react-query';
+import { ticketsService } from '../../../services/api';
+
+export function useCreateTicket() {
+  return useMutation(ticketsService.addTicket);
+}
---
+++ src/hooks/api/tickets/useDeleteTicket.ts
@@ -0,0 +1,6 @@
+import { useMutation } from 'react-query';
+import { ticketsService } from '../../../services/api';
+
+export function useDeleteTicket() {
+  return useMutation(ticketsService.deleteTicket);
+}
