26335be2e127e331e1d7da400c1aa245b4b7c985
Eduardo de Paula <eduardoibarr56@gmail.com>
2023-05-05 16:11:08 -0300

    feat: adding categories api hooks

---
+++ src/hooks/api/categories/useListCategory.ts
@@ -0,0 +1,6 @@
+import { useQuery } from 'react-query';
+import { categoriesService } from '../../../services/api';
+
+export function useListCategories() {
+  return useQuery('listCategories', categoriesService.listCategories);
+}
