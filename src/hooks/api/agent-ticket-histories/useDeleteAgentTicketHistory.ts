9f84cd97683d4c4ea2c75f26c4c4d099b1ee6f2d
Eduardo de Paula <eduardoibarr56@gmail.com>
2023-05-05 16:10:39 -0300

    feat: adding agent-ticket-histories api hooks

---
+++ src/hooks/api/agent-ticket-histories/useDeleteAgentTicketHistory.ts
@@ -0,0 +1,6 @@
+import { useMutation } from 'react-query';
+import { agentTicketHistoriesService } from '../../../services/api';
+
+export function useDeleteAgentTicketHistory() {
+  return useMutation(agentTicketHistoriesService.deleteAgentTicketHistory);
+}
