0e2b121580ce076c8f26a4a79f2e16a9817f08e3
Eduardo de Paula <eduardoibarr56@gmail.com>
2023-06-06 10:00:22 -0300

    feat: add decoding for the jwt payload

--- src/helpers/auth.ts
+++ src/helpers/auth.ts
@@ -1,5 +1,7 @@
 import { IAuth } from '../interfaces/modules/IAuth';
 import moment from 'moment';
+import jwt from 'jwt-decode';
+import { TUser } from '../types/TUser';
 
 export const ACCESS_TOKEN = 'auth-token';
 
@@ -33,8 +35,12 @@ export const setAccessToken = (data: IAuth): void => {
 	const now = moment().utc();
 	const expiresAt = moment(now).add(data.expiresIn, 'seconds');
 
+	const decodedToken: { userId: string; typeOfUser: TUser } = jwt(data.token);
+
 	const tokenData: ITokenStoraged = {
 		...data,
+		typeOfUser: decodedToken.typeOfUser,
+		userId: decodedToken.userId,
 		expirationDate: expiresAt,
 	};
 
